226a12084762c313c54f99b2405f432b1693180a
exl7954 <exl7954@nyu.edu>
2025-04-07 00:27:22 -0400

    clear file

--- src/components/SaveUpload.tsx
+++ src/components/SaveUpload.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FileButton, Button, Group, Text } from "@mantine/core";
 import { readFile } from "../util/uploadFile";
 import { mapPokemon } from "../util/mapPokemon";
@@ -8,15 +8,21 @@ export default function SaveUpload() {
   const [trainerId, setTrainerId] = useState<string>("");
   const [expected, setExpected] = useState<string>("");
 
+  const resetRef = useRef<() => void>(null);
+  const clearFile = () => {
+    setFile(null);
+    resetRef.current?.();
+  };
+
   useEffect(() => {
     if (file) {
       (async () => {
         const result = await readFile(file);
         setTrainerId(result?.data?.trainedId.toString() ?? "");
         if (result?.data?.trainedId) {
-          let poke1 = mapPokemon(result?.data?.trainedId, "Cyndaquil");
-          let poke2 = mapPokemon(result?.data?.trainedId, "Totodile");
-          let poke3 = mapPokemon(result?.data?.trainedId, "Chikorita");
+          let poke1 = mapPokemon(result?.data?.trainedId, "Chikorita");
+          let poke2 = mapPokemon(result?.data?.trainedId, "Cyndaquil");
+          let poke3 = mapPokemon(result?.data?.trainedId, "Totodile");
 
           setExpected(poke1 + ", " + poke2 + ", " + poke3);
         }
@@ -29,9 +35,12 @@ export default function SaveUpload() {
   return (
     <>
       <Group justify="center">
-        <FileButton onChange={setFile} accept="">
+        <FileButton resetRef={resetRef} onChange={setFile} accept="">
           {(props) => <Button {...props}>Upload .sav</Button>}
         </FileButton>
+        <Button disabled={!file} color="red" onClick={clearFile}>
+          Reset
+        </Button>
       </Group>
 
       {file && (
